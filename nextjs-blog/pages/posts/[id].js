c497d6bff00890ae2130dbd182b1c054c2fa7efa
Takaf19 <tk.f19ms@gmail.com>
2020-06-20 22:07:11 +0900

    Update [id].js
    各記事の内容を表示するページを作成

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,5 +1,34 @@
 import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
-export default function Post() {
-  return <Layout>...</Layout>
+// 取得した記事の内容を表示
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
+
+// Id取得処理呼び出し
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+// IDに基づき、記事の内容を取得する処理を呼び出し
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
 }
