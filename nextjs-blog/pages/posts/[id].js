6ae3d9de6287f7cead886db21c02fbc797d9919c
Takaf19 <tk.f19ms@gmail.com>
2020-06-20 22:14:55 +0900

    Update [id].js

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,15 +1,21 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
 
 // 取得した記事の内容を表示
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.id}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
@@ -25,7 +31,7 @@ export async function getStaticPaths() {
 
 // IDに基づき、記事の内容を取得する処理を呼び出し
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
